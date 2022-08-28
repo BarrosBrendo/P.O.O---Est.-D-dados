7a9caa895b25887f0648ea46a249ddbd495c55de
Harddolf <brendo.barros89@outlook.com>
2022-08-28 18:54:35 -0300

    Atividade java01
    Primeira atividade de java.

---
+++ entrega_28082022/atividade01.java
@@ -0,0 +1,20 @@
+package entrega_28082022;
+import java.util.Scanner;
+public class atividade01
+
+{
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("digite um número: ");
+        int A = input.nextInt();
+        System.out.print("digite outro número: ");
+        int B = input.nextInt();
+        int soma = A + B;
+        System.out.println("X = " + soma);
+        input.close();
+
+
+
+    }}
